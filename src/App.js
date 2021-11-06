29b89f89647a04258fd7f6aa6cdef4f563dfd598
2093ab <gil2093ab@gmail.com>
2021-11-06 14:55:32 +0900

    conflict change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-
 import './App.css';
 import Feed from "./Feed.js";
 import Home from "./Home.js";
 import AppRouter from "./routes/Router.js";
+import React, { useReducer, useEffect } from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword } from "./firebaseSetup";
+import SignUp from "./signup";
+import Login from "./login";
+
 
 function App() {
   return (
     <div className="App">
       <AppRouter></AppRouter>
     </div>
+
   );
 }
+
+
 
 export default App;
