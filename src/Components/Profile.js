f9b983a83827892086485d331c5370dce52b26ad
hyo-4 <shyo0218@naver.com>
2021-11-06 18:38:21 +0900

    Update Profile.js

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,13 +1,26 @@
-import React from "react";
+import { addDoc } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
 import pic1 from './pic1.png'
+import { dbService } from "../firebaseSetup";
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+
 
 const Profile = () => {
+
     return (
         <div>
-            <p>Profile</p>
+            <h2>Profile</h2>
             <img src={require("./pic1.png").default} />
-            <h>Name</h>
-            <p>Email</p>
+            <h1>Name</h1>
+            <h1>Email</h1>
+            <button type="submit">태그1</button>
+            <button type="submit">태그2</button>
+            <button type="submit">태그3</button>
+            <button type="submit">태그4</button>
+            <button type="submit">태그5</button>
+            <button type="submit">태그6</button>
+
+
         </div>
     );
 }
