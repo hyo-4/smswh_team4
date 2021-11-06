002dda753ce4f4042e0de94379088aa1455a61e4
hyo-4 <shyo0218@naver.com>
2021-11-06 14:30:25 +0900

    로그인,회원가입
    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
 import './App.css';
+import React, { useReducer, useEffect } from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword } from "./firebaseSetup";
+import SignUp from "./signup";
+import Login from "./login";
+
 
 function App() {
   return (
     <div className="App">
       <p>mySoMe</p>
     </div>
+
   );
 }
+
+
 
 export default App;
